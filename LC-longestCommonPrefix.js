6bb799d2fb8cb49d9039b6c15aa0523ed58762cb
hairyElephant <josecalderon353@gmail.com>
2023-03-11 08:27:55 -0500

    Got pretty close but as of right now at a loss for ideas, but i learned something new

--- LC-longestCommonPrefix.js
+++ LC-longestCommonPrefix.js
@@ -1,27 +1,16 @@
 // Date Started: 03/09/2023
 
 function findPrefix(strs){
-    let holder = []
-    for(let i = 0; i< strs.length; i++){
-        let array = strs[i].split('')
-        for(let j = 0; j < strs.length; j++){
-            if(strs[i] == strs[j]){
-                j++
-            }
-            else{
-                let other = strs[j].split('')
-                for(let k = 0; k < other.length; k++){
-                    if(array[k] == other[k]){
-                        // holder.push(array[k])
-                    }
-                }
-            }
+    let comp = strs[0]
+    for(let i = 0; i < strs.length; i++){
+        if(!strs[i].startsWith(comp)){
+            comp = comp.slice(0, -1)
         }
     }
-    
-
-    return holder
-}
-const test = ['knot','knife','know']
-
+    return comp
+    }
+let test = ['know', 'knife', 'knot']
 console.log(findPrefix(test))
+
+
+// Although not finished, this solution has taught me to use variables to store the first element of an array and then iterate through the rest of the array instead of using two 'for' loops (in certain situations this works, not all)
