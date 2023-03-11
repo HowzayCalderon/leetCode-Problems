aa316bd6324c6dbd312c205572eb35265e28c58d
hairyElephant <josecalderon353@gmail.com>
2023-03-10 19:52:38 -0500

    very rough draft

--- LC-longestCommonPrefix.js
+++ LC-longestCommonPrefix.js
@@ -1,7 +1,27 @@
 // Date Started: 03/09/2023
 
 function findPrefix(strs){
-    for(let i = 0; i<strs.length; i++){
-
+    let holder = []
+    for(let i = 0; i< strs.length; i++){
+        let array = strs[i].split('')
+        for(let j = 0; j < strs.length; j++){
+            if(strs[i] == strs[j]){
+                j++
+            }
+            else{
+                let other = strs[j].split('')
+                for(let k = 0; k < other.length; k++){
+                    if(array[k] == other[k]){
+                        // holder.push(array[k])
+                    }
+                }
+            }
+        }
     }
+    
+
+    return holder
 }
+const test = ['knot','knife','know']
+
+console.log(findPrefix(test))
